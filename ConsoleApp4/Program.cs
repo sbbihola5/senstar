a417587a1b0594cbf436ced855aa5f6d420ee9d9
sejaldave088 <sejaldave088@gmail.com>
2021-01-26 12:53:50 -0500

    program commited

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace revrsethestring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(ReverseString("the sky is blue"));
+
+
+
+        }
+        public static string ReverseString(string text)
+        {
+            char[] sArr = text.ToCharArray();
+            Array.Reverse(sArr);
+            return new string(sArr);
+                }
+
+            //for(int i=sArr.Length-1;i>-1;i--)
+            //{
+            //    reverse += sArr[i];
+            //}
+            //return reverse;
+        }
+    }
+
+
---
+++ longestword/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace longestword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            int i,length,max=0;
+            int n = 0;
+            Console.WriteLine("enter the string:");
+            text=Console.ReadLine();
+            string[] word;
+                word= text.Split(' ');
+            length = word.Length;
+          //  string words = "";
+           // int cntr = 0;
+          for (i=0; i<length;i++)
+            {
+                if(word[i].Length>max)
+                {
+                    max = word[i].Length;
+                    n = i;
+                }
+            }
+            Console.WriteLine("long string:{0} \n count character :{1} ",word[n],max);
+            Console.Read();
+        }
+    }
+}
