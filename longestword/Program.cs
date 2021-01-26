01eab8a7094d826537292f43c3d55ea507c4b38a
sejaldave088 <sejaldave088@gmail.com>
2021-01-26 13:27:37 -0500

    hii

--- longestword/Program.cs
+++ longestword/Program.cs
@@ -7,24 +7,23 @@ namespace longestword
         static void Main(string[] args)
         {
             string text;
-            int i,length,max=0;
+            int i, length, max = 0;
             int n = 0;
             Console.WriteLine("enter the string:");
-            text=Console.ReadLine();
+            text = Console.ReadLine();
             string[] word;
-                word= text.Split(' ');
+            word = text.Split(' ');
             length = word.Length;
-          //  string words = "";
-           // int cntr = 0;
-          for (i=0; i<length;i++)
+            
+            for (i = 0; i < length; i++)
             {
-                if(word[i].Length>max)
+                if (word[i].Length > max)
                 {
                     max = word[i].Length;
                     n = i;
                 }
             }
-            Console.WriteLine("long string:{0} \n count character :{1} ",word[n],max);
+            Console.WriteLine("long string:{0} \n count character :{1} ", word[n], max);
             Console.Read();
         }
     }
